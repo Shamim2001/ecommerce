4747402fff65c754cf30eab0024de1b008b9fa6d
shamim2001 <shamimahmedridoy2016@gmail.com>
2022-07-01 09:51:26 +0600

    product table migration added

--- database/migrations/2022_06_30_203128_create_products_table.php
+++ database/migrations/2022_06_30_203128_create_products_table.php
@@ -15,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 128)->unique();
+            $table->string('slug', 156)->unique();
+            $table->longText('description');
+            $table->tinyInteger('status')->default(1);
+            $table->tinyInteger('in_stock')->default(1);
+            $table->decimal('price', 8, 2);
+            $table->decimal('sale_price', 8, 2)->nullable();
             $table->timestamps();
         });
     }
